d7535c6668538725fd617eb75f2d00b3b462d568
ductoanthanh <toanth@metropolia.fi>
2018-09-05 15:15:34 +0300

    Minor fix

--- src/AutoComplete.js
+++ src/AutoComplete.js
@@ -149,7 +149,7 @@ class Example extends Component {
         getSectionSuggestions={this.getSectionSuggestions}
         theme={theme}
         // just for development
-        alwaysRenderSuggestions={true}
+        alwaysRenderSuggestions={false}
       />
     );
   }
